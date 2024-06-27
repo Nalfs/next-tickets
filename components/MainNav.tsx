e31cedc4497f039d0b9586e80c5909ca53dfb38d
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2024-06-27 16:15:40 +0200

    adding component styling for links

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
 import React from "react";
 import { ToggleMode } from "./ToggleMode";
+import MainNavLinks from "./MainNavLinks";
 
 const MainNav = () => {
   return (
     <div className="flex justify-between">
-      <div>
-        <Link href="/">Dashboard</Link>
-        <Link href="/tickets">Tickets</Link>
-        <Link href="/users">Users</Link>
-        <Link href="/">Dashboard</Link>
-      </div>
+      <MainNavLinks />
+
       <div className="flex items-center gap-2">
         <Link href="/">Logout</Link>
         <ToggleMode />
