ece2c889bac15c3d7d6642b5f4e398ecbab6a29b
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2024-07-14 12:15:41 +0200

    priority component

--- app/tickets/DataTable.tsx
+++ app/tickets/DataTable.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/table";
 import React from "react";
 import TicketStatusbadge from "@/components/TicketStatusbadge";
+import TicketPriority from "@/components/TicketPriority";
 
 interface DataTableProps {
   tickets: Ticket[];
@@ -25,7 +26,9 @@ const DataTable = ({ tickets }: DataTableProps) => {
               <TableHead>
                 <div className="flex justify-center">Status </div>
               </TableHead>
-              <TableHead>Priority</TableHead>
+              <TableHead>
+                <div className="flex justify-center">Priority</div>
+              </TableHead>
               <TableHead>Created at</TableHead>
             </TableRow>
           </TableHeader>
@@ -39,7 +42,11 @@ const DataTable = ({ tickets }: DataTableProps) => {
                       <TicketStatusbadge status={ticket.status} />
                     </div>
                   </TableCell>
-                  <TableCell>{ticket.priority}</TableCell>
+                  <TableCell>
+                    <div className="flex justify-center">
+                      <TicketPriority priority={ticket.priority} />
+                    </div>
+                  </TableCell>
                   <TableCell>
                     {ticket.createdAt.toLocaleDateString("sv-SE", {
                       year: "2-digit",
