9efa2941d4e6816322fff7f063f58ec7ffe56cb4
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2024-11-06 20:29:34 +0100

    short comment

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
+// A React component that wraps children with Next.js ThemeProvider for theme management.
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
